6b8e82d14ccc88a131348ba9d682e43ae93ac4b4
Randy Stauner <randy@r4s6.net>
2021-03-08 20:39:05 -0700

    Parse file into list of records

--- src/net/r4s6/woni/parse.clj
+++ src/net/r4s6/woni/parse.clj
@@ -1,14 +1,54 @@
 (ns net.r4s6.woni.parse
   (:require [clojure.string :as string]))
 
+(def default-columns
+  ["LastName"
+   "FirstName"
+   "Email"
+   "FavoriteColor"
+   "DateOfBirth"])
 
 (def separator-re #"[ ,|]+")
+
+(defn make-record
+  "Returns a map representing a record.
+  Returns nil if the record isn't valid."
+  [columns values]
+  (when (= (count columns) (count values))
+    (zipmap columns values)))
 
 ; NOTE: If we wanted to use clojure.data.csv:
 ; (let [[delim] (seq (or (re-find #"[|,]" line) " "))]
 ;   (-> line (csv/read-csv :separator delim) first (->> (map string/trim))))
 
+
 (defn parse-line
   "Parse input string into fields."
   [line]
   (string/split line separator-re))
+
+(defn header?
+  "Determine if list appears to be column headings."
+  [xs]
+  (some (set default-columns) xs))
+
+(defn parse-records
+  "Parse list of strings into list of records."
+  [lines]
+  (let [head (some-> lines
+                     first
+                     parse-line)
+        [columns lines] (if (and head (header? head))
+                          [head (rest lines)]
+                          [default-columns lines])]
+    (->> lines
+         (map #(make-record columns (parse-line %)))
+         (filter some?))))
+
+(defn parse-file
+  "Parse file into list of records."
+  [file]
+  (-> file
+      slurp
+      string/split-lines
+      parse-records))
--- test/net/r4s6/woni/parse_test.clj
+++ test/net/r4s6/woni/parse_test.clj
@@ -11,4 +11,41 @@
            (p/parse-line "a, b, c"))))
   (testing "space delimited"
     (is (= ["a" "b" "c"]
-           (p/parse-line "a b c")))))
+           (p/parse-line "a b c"))))
+  (testing "unknown format"
+    (is (= ["a.b.c"]
+           (p/parse-line "a.b.c")))))
+
+(deftest parse-records
+  (testing "parse empty list"
+    (is (= []
+           (p/parse-records []))))
+
+  (testing "parse with header row"
+    (is (= [{"LastName" "l" "FirstName" "f"}
+            {"LastName" "a" "FirstName" "b"}]
+           (p/parse-records ["LastName, FirstName"
+                             "l, f" "a, b"]))))
+
+  (testing "parse with custom fields"
+    (is (= [{"LastName" "l" "FirstName" "f" "Mood" "happy"}
+            {"LastName" "a" "FirstName" "b" "Mood" "sad"}]
+           (p/parse-records ["LastName, FirstName, Mood"
+                             "l, f, happy"
+                             "a, b, sad"]))))
+
+  (testing "parse without header row"
+    (is (= [{"LastName" "l" "FirstName" "f" "Email" "e" "FavoriteColor" "fc" "DateOfBirth" "dob"}
+            {"LastName" "a" "FirstName" "b" "Email" "c" "FavoriteColor" "d" "DateOfBirth" "e"}]
+           (p/parse-records ["l, f, e, fc, dob"
+                             "a, b, c, d, e"]))))
+
+  (testing "drops rows that do not parse correctly"
+    (is (= [{"LastName" "a" "FirstName" "b" "Email" "c" "FavoriteColor" "d" "DateOfBirth" "e"}]
+           (p/parse-records ["a.b.c"
+                             "a, b, c, d, e"])))
+    (is (= [{"LastName" "a" "FirstName" "b"}]
+           (p/parse-records ["LastName, FirstName"
+                             "a.b.c"
+                             "a, b, c, d, e"
+                             "a, b"])))))
